4b776d1dbbe10ecccfe27baa247acd2c0529c519
Marta Rodriguez <rodriguez.martin.marta@gmail.com>
2015-07-09 16:26:53 +0200

    repository pattern

---
+++ CleanMercadona/app/src/main/java/marta/rodriguez/mercadonaapp/mercadona/repository/SupermarketDataRepository.java
@@ -0,0 +1,17 @@
+package marta.rodriguez.mercadonaapp.mercadona.repository;
+
+import java.util.List;
+
+import marta.rodriguez.mercadonaapp.mercadona.model.Supermarket;
+
+/**
+ * Created by martarodriguez on 9/7/15.
+ */
+public class SupermarketDataRepository implements SupermarketRepository {
+
+
+    @Override
+    public List<Supermarket> getSupermarkets() {
+        return null;
+    }
+}
---
+++ CleanMercadona/app/src/main/java/marta/rodriguez/mercadonaapp/mercadona/repository/SupermarketRepository.java
@@ -0,0 +1,13 @@
+package marta.rodriguez.mercadonaapp.mercadona.repository;
+
+import java.util.List;
+
+import marta.rodriguez.mercadonaapp.mercadona.model.Supermarket;
+
+/**
+ * Created by martarodriguez on 9/7/15.
+ */
+public interface SupermarketRepository {
+
+    List<Supermarket> getSupermarkets();
+}
